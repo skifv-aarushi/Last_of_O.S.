b3541471383e1b0edca3daa944ec33a3fb136ded
Aarushi <aarushijha2505@gmail.com>
2026-01-18 23:33:52 +0530

    minor change

--- Backend/Tasks/task.py
+++ Backend/Tasks/task.py
@@ -2,4 +2,4 @@ def calculate_task_score(slider1: float = 50.0, slider2: float = 50.0, slider3: 
     
     average_score = (slider1 + slider2 + slider3) / 3
     
-    return round(average_score, 2)
+    return round(average_score, 3)
