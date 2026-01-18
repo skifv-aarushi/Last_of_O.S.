40b06478590dcb56f365b71bfab1e6c882fae79e
Aarushi <aarushijha2505@gmail.com>
2026-01-18 23:32:58 +0530

    Added sample task

---
+++ Backend/Tasks/task.py
@@ -0,0 +1,5 @@
+def calculate_task_score(slider1: float = 50.0, slider2: float = 50.0, slider3: float = 50.0) -> float:
+    
+    average_score = (slider1 + slider2 + slider3) / 3
+    
+    return round(average_score, 2)
